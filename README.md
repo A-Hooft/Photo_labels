3614007bd295beb81b868fbe9d9100112b414fb7
Alex_calcua_2 <vsc20646@r0c03cn1>
2024-07-29 20:46:30 +0200

    full update

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# Photo_labels
-For standardised processing of (genomic) specimens, standard labels are needed for inclusion in all photos. 
+# Label Generator
+
+This project is designed to automate the creation of uniform labels for various purposes, and originally designed for producing labels for in photos of biolgoical speciemens. It utilizes the `blabel` library to generate PDF labels based on a given configuration file. labels can be generated based on a sequenctial numerical identifier or list. optionally an automatically rendered QRcode can be included to allow for automatic content classifcation of photos. A scale bar and 4 preset sizes are defined, but all CSS elements and labels contents are in theory customisable
+
+
+## Installation
+
+To set up the project and install the necessary dependencies, follow these steps:
+
+### 1) Clone the repository:
+
+git clone git@github.com:A-Hooft/Photo_labels.git
+
+### 2) Navigate to the project directory:
+
+cd Photo_labels
+
+### 3) Install the required packages using pip:
+
+pip install -r requirements.txt
+
+their may be issues with the weasyprint package, in that case consult weasyprint installation steps https://doc.courtbouillon.org/weasyprint/stable/first_steps.html#installation
+
+
+## Configuration File
+
+The configuration file `Config/write_labels.yaml` includes several parameters;
+
+- `label_size`: Size of labels (xsmall/small/medium/large/custom)
+- `qr_code`: Enable or disable QR code generation
+- `scale_bar`: Enable or disable scale bar
+- `Project_name`: Name of the project - included in top right hand corner of label
+- `filename`: Name of the output file - 
+- `sequential_labels`: Enable or disable sequential label generation
+- `Prefix`: Optional prefix for all specimen numbers
+- `Suffix`: Optional suffix for all specimen numbers
+- `Range`: Specimen number range (used if `sequential_labels` is true)
+- `Labels_list`: List of labels (used if `sequential_labels` is false)
+
+
+## Generate labels
+
+To generate the labels, execute the following command to run the python script:
+
+python write_labels.py
+
+This will read the configuration from write_labels.yaml, generate the labels according to the specified parameters, and save the output as a PDF file.
--- write_labels.py
+++ write_labels.py
@@ -2,13 +2,16 @@ from blabel import LabelWriter
 import yaml
 import os
 
-# Initialize the LabelWriter with the template and stylesheets
-label_writer = LabelWriter(
-    "A4_item_template.html", items_per_page=1, default_stylesheets=("A4_label_style.css",)
-)
+STYLESHEETS = {
+    'xsmall': "CSS_template/A4_label_style_xsmall.css",
+    'small': "CSS_template/A4_label_style_small.css",
+    'medium': "CSS_template/A4_label_style_medium.css",
+    'large': "CSS_template/A4_label_style_large.css",
+    'custom': "CSS_template/A4_label_style_custom.css"
+}
 
 # Function to load configuration from a YAML file
-def load_config(config_path='config.yaml'):
+def load_config(config_path=os.path.join("Config", "write_labels.yaml")):
     with open(config_path, 'r') as file:
         config = yaml.safe_load(file)
     return config
@@ -17,7 +20,7 @@ def load_config(config_path='config.yaml'):
 config = load_config()
 
 # Check if all required keys are present in the config
-required_keys = ['label_size', 'qr_code', 'scale_bar', 'Project_name', 'sequential_labels', 'Prefix', 'Suffix', 'Range', 'filename']
+required_keys = ['label_size', 'qr_code', 'scale_bar', 'project_name', 'sequential_labels', 'Prefix', 'Suffix', 'Range', 'filename']
 for key in required_keys:
     if key not in config:
         raise KeyError(f"Missing required config key: {key}")
@@ -26,19 +29,31 @@ for key in required_keys:
 label_size = config['label_size']
 qr_code = config['qr_code']
 scale_bar = config['scale_bar']
-project_name = config['Project_name']
+project_name = config['project_name']
 sequential_labels = config['sequential_labels']
-prefix = config['Prefix']
-suffix = config['Suffix']
-range_end = config['Range']
+Prefix = config['Prefix']
+Suffix = config['Suffix']
 filename = config['filename']
+range_str = config['Range']
+rangestart, rangeend = map(int, range_str.split('-'))
+
+# Select the stylesheet based on label size
+stylesheet = STYLESHEETS.get(label_size, "CSS&template/A4_label_style_custom.css")
+
+# Initialize the LabelWriter with the template and selected stylesheet
+label_writer = LabelWriter(
+    "CSS_template/A4_item_template.html", items_per_page=1, default_stylesheets=(stylesheet,)
+)
 
 # Define the output directory and ensure it exists
 output_dir = "label_sheets/"
 os.makedirs(output_dir, exist_ok=True)
 
 # Generate sample IDs based on the specified range, prefix, and suffix
-sample_ids = [f"{prefix}{i}{suffix}" for i in range(range_end)]
+if sequential_labels:
+    sample_ids = [f"{Prefix}{str(i)}{Suffix}" for i in range(rangestart, rangeend + 1)]
+else:
+    sample_ids = config['Labels_list']
 
 # Prepare the records for label writing
 records = []
@@ -46,7 +61,10 @@ for sample_id in sample_ids:
     record = {
         'sample_id': sample_id,
         'project_label': project_name,
-        'sample': sample_id
+        'sample': sample_id,
+        'qr_code': qr_code,
+        'scale_bar': scale_bar,
+        'label_size': label_size
     }
     if qr_code:
         record['QRValue'] = f"{sample_id};{project_name}"
